2aaa9f155a524bff314494b6caaf1ac4d5982f6a
Erwan Decoster <wanerd2@gmail.com>
2022-12-05 13:36:32 +0100

    correction plantage lier au google analytic

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,9 +1,9 @@
-import VueGtag from 'vue-gtag-next'
-const config = useRuntimeConfig();
+import VueGtag from 'vue-gtag'
+
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(VueGtag, {
-    property: {
-      id: config.GTAGID,
-    }
-  })
+    nuxtApp.vueApp.use(VueGtag, {
+        config: {
+            id: 'G-0332DJY37K',
+        },
+    }, nuxtApp.$router)
 })
