46f14092982fbc2ffd96237fd989662030c423cd
Erwan Decoster <wanerd2@gmail.com>
2023-07-29 20:27:40 +0200

    remove pupop correctly

--- app.vue
+++ app.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="text-eerie-black bg-white dark:bg-eerie-black dark:text-white min-h-[100vh] h-full">
     <!-- <button v-if="!showModal" @click="showModal = true" class="z-50 fixed top-1 left-1 p-2 bg-almond dark:bg-dark-almond bg-opacity-50 hover:bg-opacity-100 duration-200 rounded text-white">Site en construction infos</button> -->
-    <PopUp 
+    <!-- <PopUp 
       v-if="showModal" 
       @close="showModal = false"
-    >
+    > -->
       <!-- <p class="text-xl">Site en construction</p> -->
       <!-- <div>
         <p>Site en cours de construction, certaine fonctionctionnaliteé ne sont pas encore au point, n'hesitez pas a donner votre avis sur votre experience. <a class="text-dark-almond dark:text-almond hover:underline" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLScrA6ZsyNeKY2iKPFHmfDSfFC_EmouGnmTjgDRDo-T1ZBvuCw/viewform?usp=sf_link">Google Form</a></p>
       </div> -->
-    </PopUp>
+    <!-- </PopUp> -->
     <NavBar />
     <NuxtPage />
     <Footer />
