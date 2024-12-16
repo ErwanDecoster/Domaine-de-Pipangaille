8ce26610d8822075914becb2098588d7cf61a8c6
ErwanDecoster <wanerd2@gmail.com>
2024-12-16 15:58:58 +0100

    use Composition API instead of Options API on app.vue

--- app.vue
+++ app.vue
@@ -1,38 +1,40 @@
-<script>
-export default {
-  created() {
-    const head = useLocaleHead({
-      addDirAttribute: true,
-      identifierAttribute: 'id',
-      addSeoAttributes: true,
-    })
-    const title = this.$t('index.meta.title')
-    useHead({
-      title,
-      htmlAttrs: {
-        lang: head.value.htmlAttrs.lang,
-        dir: head.value.htmlAttrs.dir,
-      },
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'robots', content: 'index, follow' },
-        { name: 'theme-color', content: '#ffffff' },
-      ],
-    })
+<script setup>
+const { t } = useI18n()
+
+const head = useLocaleHead({
+  addDirAttribute: true,
+  identifierAttribute: 'id',
+  addSeoAttributes: true,
+})
+
+useHead({
+  title: t('index.meta.title'),
+  htmlAttrs: {
+    lang: head.value.htmlAttrs.lang,
+    dir: head.value.htmlAttrs.dir,
   },
-  mounted() {
-    const style = 'background: #1A1D1A; color: #eee; border-radius: 4px; padding: 2px 10px; border: 1px solid #595859; font-size: 16px;'
-    console.log('%cWebsite designed and developed by Erwan Decoster www.erwan-decoster.com in Nuxt 🤍\nGithub repo : https://github.com/ErwanDecoster/Domaine-de-Pipangaille', style)
-  },
-}
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { name: 'robots', content: 'index, follow' },
+    { name: 'theme-color', content: '#ffffff' },
+  ],
+})
+
+onMounted(() => {
+  const style = 'background: #1A1D1A; color: #eee; border-radius: 4px; padding: 2px 10px; border: 1px solid #595859; font-size: 16px;'
+  console.log(
+    '%cWebsite designed and developed by Erwan Decoster www.erwan-decoster.com in Nuxt 🤍\nGithub repo : https://github.com/ErwanDecoster/Domaine-de-Pipangaille',
+    style,
+  )
+})
 </script>
 
 <template>
   <div class="text-eerie-black bg-white dark:bg-eerie-black dark:text-white min-h-[100vh] h-full">
-    <NavBar />
-    <NuxtPage />
-    <LazyFooter />
+    <!-- <NavBar /> -->
+    <!-- <NuxtPage /> -->
+    <!-- <LazyFooter /> -->
   </div>
 </template>
 
@@ -48,23 +50,9 @@ html {
     font-family: 'Nunito';
 }
 
-/* h1, h2, h3, h4, h5 {
-    font-family: "Lora";
-    src: local("Lora"),
-     url(~/assets/font/Lora-Regular.ttf) format("truetype");
-} */
 h1, h2, h3, h4, h5 {
     font-family: "WollstonecraftRomance";
     src: local("WollstonecraftRomance"),
      url(~/assets/font/WollstonecraftRomance-Regular.ttf) format("truetype");
 }
-/* .page-enter-active,
-.page-leave-active {
-  transition: all 0.4s;
-}
-.page-enter-from,
-.page-leave-to {
-  opacity: 0;
-  filter: blur(1rem);
-} */
 </style>
